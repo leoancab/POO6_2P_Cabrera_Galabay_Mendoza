3dd9cca4cc7b7ea80d77e9e50b7d06ecd018a2a2
Leonel Cabrera <leoancab@espol.edu.ec>
2022-01-30 01:01:04 -0500

    Se implemento la clase Cita para llenar la tabla de citas

--- Proyecto_POO_2P/src/main/java/com/pooespol/proyecto_poo_2p/modelo/Cita.java
+++ Proyecto_POO_2P/src/main/java/com/pooespol/proyecto_poo_2p/modelo/Cita.java
@@ -5,10 +5,60 @@
  */
 package com.pooespol.proyecto_poo_2p.modelo;
 
+import java.io.Serializable;
+
 /**
  *
  * @author cgala
  */
-public class Cita {
-    
+public class Cita implements Serializable {
+
+    private String nombres;
+    private String apellidos;
+    private String fechas;
+    private String nroSolicitud;
+
+    public Cita(String nombres, String apellidos, String fechas, String nroSolicitud) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fechas = fechas;
+        this.nroSolicitud = nroSolicitud;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getFechas() {
+        return fechas;
+    }
+
+    public void setFechas(String fechas) {
+        this.fechas = fechas;
+    }
+
+    public String getNroSolicitud() {
+        return nroSolicitud;
+    }
+
+    public void setNroSolicitud(String nroSolicitud) {
+        this.nroSolicitud = nroSolicitud;
+    }
+
+    @Override
+    public String toString() {
+        return "Cita{" + "nombres=" + nombres + ", apellidos=" + apellidos + ", fechas=" + fechas + ", nroSolicitud=" + nroSolicitud + '}';
+    }
 }
